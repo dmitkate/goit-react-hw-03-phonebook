a24d5cf90a13fa1697461707298223e92ae9e6df
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-11-02 18:39:54 +0200

    add local storage

--- src/components/Formin.jsx
+++ src/components/Formin.jsx
@@ -21,7 +21,7 @@ export class FormIn extends Component {
   handleSubmit = evt => {
     evt.preventDefault();
     this.props.onSubmit(this.state);
-    //console.log(this.state);
+
     this.formReset();
   };
 
@@ -55,34 +55,6 @@ export class FormIn extends Component {
           </label>
           <button type="submit">Add Contact</button>
         </form>
-        {/* <h1>Phone books</h1>
-        <Formik initialValues={this.state} onSubmit={this.handleSubmit}>
-          <Form action="">
-            <label htmlFor="name">
-              Name
-              <Field
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-            <label htmlFor="number">
-              Number
-              <Field
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-            <button type="submit">Add contact</button>
-          </Form>
-        </Formik> */}
       </>
     );
   }
